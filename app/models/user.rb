19fc2866ae5229192d022ff133548eaf42cc9d4c
vazquezy88 <vazquezy88@gmail.com>
2014-02-22 12:56:23 -0600

    Rails Girls Houston Project

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+has_many :posts
+
+validates_presence_of :name
+validates_presence_of :email
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Railsgirlshouston::Application.routes.draw do
   resources :users, :posts
 
+root to: 'static#home'
+
 end
